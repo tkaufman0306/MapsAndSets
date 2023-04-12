359f4e79cc1c0521b0b3fd9254cc0d2108ef8823
Taylor <taylorkkaufman@gmail.com>
2023-04-11 20:14:51 -0600

    added files to repo

---
+++ exercise.js
@@ -0,0 +1,30 @@
+
+
+new Set([1, 1, 2, 2, 3, 4]); // Set(4) {1, 2, 3, 4}
+
+[...new Set("referee")].join("") // 'ref'
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1, 2, 3], false); // {Array(3) => true, Array(3) => false}
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+function isVowel(char){
+return 'aeiou'.includes(char);
+}
+
+function vowelCount(str) {
+    const vowelMap = new Map();
+    for (let char of str) {
+        let lowerCaseChar = char.toLowerCase()
+        if (isVowel(lowerCaseChar)) {
+            if (vowelMap.has(lowerCaseChar)) {
+                vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+            } else {
+                vowelMap.set(lowerCaseChar, 1);
+            }
+        }
+    }
+    return vowelMap;
+}
